1618cb0102b26c1da01bccaab75f90c9f01a8f78
Adel Faruque <Adelfaruque1@gmail.com>
2025-07-05 06:25:38 -0400

    switched the popup message if the user is on google maps than if they weren't

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const catElements = document.querySelectorAll(".cat");
-  const last = parseInt(sessionStorage.getItem("lastCatIndex") ?? -1);
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    const currentTab = tabs[0];
+    const pageUrl = currentTab.url;
 
-  let randomIndex;
-  do {
-    randomIndex = Math.floor(Math.random() * catElements.length);
-  } while (randomIndex === last && catElements.length > 1);
+    //console.warn("User's active tab URL:", pageUrl);
+    const catElements = document.querySelectorAll(".cat");
+    const last = parseInt(localStorage.getItem("lastCatIndex") ?? 0);
 
-  // Store current index for next load to avoid repeating cats
-  sessionStorage.setItem("lastCatIndex", randomIndex);
+    let randomIndex;
+    do {
+        randomIndex = Math.floor(Math.random() * catElements.length);
+    } while (randomIndex === last);
 
-  // Show the randoly selected cat
-  catElements[randomIndex].style.display = "block";
+    // Store current index for next load
+    localStorage.setItem("lastCatIndex", randomIndex);
+
+    // Show the selected cat
+    if (pageUrl.startsWith("https://www.google.com/maps")) {
+        document.getElementsByClassName("content")[0].textContent = "Click on a location to explore it on community platforms."
+    } else {
+        catElements[randomIndex].style.display = "block";
+    }
+    //console.warn("last index: ", last, " current index: ", randomIndex);
 });
